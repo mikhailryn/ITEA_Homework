630c4b0773c188721df146d27dcd208753c9651e
Mdryn <mdryndin@gmail.com>
2019-09-18 19:48:37 +0300

    Homework

---
+++ Les2_01_Ryndin.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Lesson2_01_Ryndin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int ResultOfDividing;
+            int Remainder;
+
+            int VisitorMoney = 100;
+
+            int BeerPrice1 = 80;
+            int BeerPrice2 = 70;
+            int BeerPrice3 = 60;
+            int BeerPrice4 = 7;
+
+            string BeerName1 = "Leffe";
+            string BeerName2 = "Hoegarden";
+            string BeerName3 = "StellaArtois";
+            string BeerName4 = "Obolon";
+
+            Console.WriteLine($"У гостя {VisitorMoney} грн -> ви можете купити:");
+
+            ResultOfDividing = VisitorMoney / BeerPrice1;
+            Remainder = VisitorMoney % BeerPrice1;
+            Console.WriteLine($"{ResultOfDividing} {BeerName1}, решта {Remainder}");
+
+            ResultOfDividing = VisitorMoney / BeerPrice2;
+            Remainder = VisitorMoney % BeerPrice2;
+            Console.WriteLine($"{ResultOfDividing} {BeerName2}, решта {Remainder}");
+
+            ResultOfDividing = VisitorMoney / BeerPrice3;
+            Remainder = VisitorMoney % BeerPrice3;
+            Console.WriteLine($"{ResultOfDividing} {BeerName3}, решта {Remainder}");
+
+            ResultOfDividing = VisitorMoney / BeerPrice4;
+            Remainder = VisitorMoney % BeerPrice4;
+            Console.WriteLine($"{ResultOfDividing} {BeerName4}, решта {Remainder}");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Les2_02_Ryndin.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Lesson2_02_Ryndin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number:");
+            float money = Convert.ToSingle(Console.ReadLine());
+            int discount1 = 1;
+            float discount2 = 0.98F;
+            float discount3 = 0.97F;
+            float discount4 = 0.95F;
+            float Pay;
+
+            if (money >= 0 && money < 300)
+            {
+                Pay = money * discount1;
+                Console.WriteLine($"No discount, total {Pay}");
+                Console.ReadKey();
+            }
+            else if (money >= 300 && money < 400)
+            {
+                Pay = money * discount2;
+                Console.WriteLine($"Your discount's 2%, total {Pay}");
+                Console.ReadKey();
+            }
+            else if (money >= 400 && money < 500)
+            {
+                Pay = money * discount3;
+                Console.WriteLine($"Your discount's 3%, total {Pay}");
+                Console.ReadKey();
+            }
+            else if (money >= 500 && money >= 500)
+            {
+                Pay = money * discount4;
+                Console.WriteLine($"Your discount's 5%, total {Pay}");
+                Console.ReadKey();
+
+            }
+        }
+    }
+}
