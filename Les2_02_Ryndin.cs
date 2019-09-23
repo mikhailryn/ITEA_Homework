09972ca318638f7e6d6bebc8c5f1ce17518595fc
Mdryn <mdryndin@gmail.com>
2019-09-23 13:27:38 +0300

    with two variables

--- Les2_02_Ryndin.cs
+++ Les2_02_Ryndin.cs
@@ -1,44 +1,36 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace Lesson2_02_Ryndin
+namespace ConsoleApp2
 {
     class Program
     {
         static void Main(string[] args)
         {
+            double result;
+            double discount = 0;
+
             Console.WriteLine("Enter a number:");
-            float money = Convert.ToSingle(Console.ReadLine());
-            int discount1 = 1;
-            float discount2 = 0.98F;
-            float discount3 = 0.97F;
-            float discount4 = 0.95F;
-            float Pay;
+            double var1 = Convert.ToDouble(Console.ReadLine());
 
-            if (money >= 0 && money < 300)
+            if (var1 > 300 && var1 <= 400)
+                discount = 0.02;
+            else
             {
-                Pay = money * discount1;
-                Console.WriteLine($"No discount, total {Pay}");
-                Console.ReadKey();
+                if (var1 > 400 && var1 <= 500)
+                    discount = 0.03;
+                else
+                    if (var1 > 500)
+                    discount = 0.05;
             }
-            else if (money >= 300 && money < 400)
-            {
-                Pay = money * discount2;
-                Console.WriteLine($"Your discount's 2%, total {Pay}");
-                Console.ReadKey();
-            }
-            else if (money >= 400 && money < 500)
-            {
-                Pay = money * discount3;
-                Console.WriteLine($"Your discount's 3%, total {Pay}");
-                Console.ReadKey();
-            }
-            else if (money >= 500 && money >= 500)
-            {
-                Pay = money * discount4;
-                Console.WriteLine($"Your discount's 5%, total {Pay}");
-                Console.ReadKey();
 
-            }
+            result = var1 - var1 * discount;
+            Console.WriteLine($"сума до сплати {result}" +
+            $", знижка {discount}");
+            Console.ReadKey();
         }
     }
 }
