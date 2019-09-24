8eae02e217b3bfdd79f5db716450d9c6996831aa
Mdryn <mdryndin@gmail.com>
2019-09-24 15:27:04 +0300

    corrected

--- Les3_01.cs
+++ Les3_01.cs
@@ -1,60 +1,77 @@
-#region Using directives 
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using static System.Console;
-using static System.Convert;
-using static System.Char;
-#endregion
 
-namespace ConsoleApp2
+namespace Lesson3_01_Ryndin
 {
     class Program
     {
+
         static void Main(string[] args)
         {
-            int numQuantity = 0, evenSum = 0, oddQuantity = 0, multipleOf3 = 0, intNumber;
-
-            string myString = ReadLine();
-            char[] myChars = myString.ToCharArray();
-            WriteLine($"Количество символов в строке2: {myChars.Length}");
-
-            int i = 0;
-            while (i < myChars.Length)
+           Console.WriteLine("Введите число");
+           int Number = Convert.ToInt32(Console.ReadLine());
+           Console.WriteLine("Введите номер операции: 1.Количество цифр в числе  2.Сумма четных цифр  3.Количество кратных трем");
+           string operation = Console.ReadLine();
+            switch (operation)
             {
-                if (IsDigit(myChars[i]))
+                case "1":
+                    quantity();
+                    break;
+                case "2":
+                    sum();
+                    break;
+                case "3":
+                    multiple();
+                    break;
+                default:
+                    Console.WriteLine("Вы выбрали неизвестную операцию");
+                    Console.ReadKey();
+                    break;
+            }
+            void quantity()
+            {
+                int i = 0;
+                while (Number != 0)
                 {
-                    ++numQuantity;
-                    intNumber = ToInt32(GetNumericValue(myChars[i]));
-
-                    if (intNumber % 2 == 0)
-                    {
-                        //WriteLine($"Четная цифра: {intNumber}"); 
-                        evenSum += intNumber;
-                    }
-                    else
-                    {
-                        //WriteLine($"Нечетная цифра: {intNumber}"); 
-                        ++oddQuantity;
-                    }
-
-                    if (intNumber % 3 == 0)
-                    {
-                        //WriteLine($"Цифра, кратная трем: {intNumber}"); 
-                        ++multipleOf3;
-                    }
+                    i++;
+                    Number /= 10;
                 }
-                i++;
+                Console.WriteLine($"Количество цифр в числе {i} ");
+                Console.ReadKey();
+            }
+             void sum()
+            {
+                int i = 0;
+                while (Number > 0)
+                {
+                    if ((Number % 10) % 2 == 0)
+                    {
+                        i = i + Number % 10;
+                    }
+                    Number = Number / 10;
+                }
+                Console.WriteLine($"Сумма четных цифр в числе {i} ");
+                Console.ReadKey();
+            }
+             void multiple()
+            {
+                int i = 0;
+                while (Number > 0)
+                {
+
+                    if ((Number % 10) % 3 == 0)
+                    {
+                        i++;
+                    }
+                    Number = Number / 10;
+                }
+                Console.WriteLine($"Количество цифр кратных трем в числе {i} ");
+                Console.ReadKey();
             }
 
-            WriteLine($"Количество цифр в строке: {numQuantity}");
-            WriteLine($"Сумма четных цифр в числе: {evenSum}");
-            //WriteLine($"Количество нечетных цифр: {oddQuantity}"); 
-            WriteLine($"Количество цифр, кратных трем: {multipleOf3}");
-
-            ReadLine();
+        }
         }
     }
-}
